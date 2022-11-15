77d635716bf35a39e68f0c022f841525a4995154
sanjiv <sanjiv.katiyar@gmail.com>
2022-11-14 17:26:58 -0700

    ok

--- src/main/java/com/ocp/java/_10/streamsandlambda/streams/ParallelStream.java
+++ src/main/java/com/ocp/java/_10/streamsandlambda/streams/ParallelStream.java
@@ -7,9 +7,16 @@ public class ParallelStream {
     public static void main(String[] args) {
 
         int sum = Stream.of(10,20,30,40,50,60)
-                .parallel()
+                .parallel()                                        //Parallel processing
                 .mapToInt(Integer::intValue)
                 .sum();
         System.out.println(sum);
+
+        System.out.println(Stream.iterate(1, n-> n+1)
+                .limit(10000).reduce(0, (a,b) -> a+b));
+
+        System.out.println(Stream.iterate(1, n-> n+1)
+                .parallel()
+                .limit(10000).reduce(0, (a,b) -> a+b));
     }
 }
