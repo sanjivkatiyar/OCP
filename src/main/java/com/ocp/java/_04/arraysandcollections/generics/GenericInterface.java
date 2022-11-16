edd31e28d3396d1d3f0aef0b3b960287c8f67357
sanjiv <sanjiv.katiyar@gmail.com>
2022-11-15 20:32:43 -0700

    ok

--- src/main/java/com/ocp/java/_04/arraysandcollections/generics/GenericInterface.java
+++ src/main/java/com/ocp/java/_04/arraysandcollections/generics/GenericInterface.java
@@ -3,7 +3,9 @@ package com.ocp.java._04.arraysandcollections.generics;
 public class GenericInterface {
     public static void main(String[] args) {
         new MoveFeline().move(new Cat());
+        // new MoveFeline().move(new Dog());           // Compilation Error
         new MoveCanine().move(new Dog());
+        // new MoveCanine().move(new Cat());           // Compilation Error
         new SomeMovable<Dog>().move(new Dog());
         new SomeMovable<Cat>().move(new Cat());
     }
