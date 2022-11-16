3e909e844e74bdb5db038c68604ba3909fa52db2
sanjiv <sanjiv.katiyar@gmail.com>
2022-11-15 20:20:09 -0700

    ok

---
+++ src/main/java/com/ocp/java/_04/arraysandcollections/generics/GenericClass.java
@@ -0,0 +1,25 @@
+package com.ocp.java._04.arraysandcollections.generics;
+
+class MyGeneric<T> {
+    T t;
+
+    MyGeneric(T t) {
+        this.t = t;
+    }
+
+    T getT() {
+        return t;
+    }
+}
+
+public class GenericClass {
+
+    public static void main(String[] args) {
+        MyGeneric<String> g = new MyGeneric<>("SK");
+        System.out.println(g.getT());
+
+        MyGeneric<Integer> g2 = new MyGeneric<>(3);
+        System.out.println(g2.getT());
+    }
+}
+
---
+++ src/main/java/com/ocp/java/_04/arraysandcollections/generics/GenericInterface.java
@@ -0,0 +1,26 @@
+package com.ocp.java._04.arraysandcollections.generics;
+
+public class GenericInterface {
+    public static void main(String[] args) {
+        new MoveFeline().move(new Cat());
+        new MoveCanine().move(new Dog());
+        new SomeMovable<Dog>().move(new Dog());
+        new SomeMovable<Cat>().move(new Cat());
+    }
+}
+
+interface Moveable<T>{
+    void move(T t);
+}
+class MoveFeline implements Moveable<Cat>{
+    @Override
+    public void move(Cat cat) {}
+}
+class MoveCanine implements Moveable<Dog>{
+    @Override
+    public void move(Dog dog) {}
+}
+class SomeMovable<U> implements Moveable<U>{
+    @Override
+    public void move(U u) {}
+}
