635352f83de324e8ba6ed0af91d54bc3c4063ac6
sanjiv <sanjiv.katiyar@gmail.com>
2022-11-19 14:16:48 -0700

    ok

---
+++ src/main/java/com/ocp/java/mock/test1/Q15.java
@@ -0,0 +1,21 @@
+package com.ocp.java.mock.test1;
+
+public class Q15 {
+
+    int value = 1;
+    Q15 link;
+    public Q15(int val){ this.value = val; }
+    public static void main(String[] args){
+        final var a = new Q15(5);
+        var b = new Q15(10);
+        a.link = b;
+        b.link = setIt(a, b);                                 // this is setting a.link to null
+        System.out.println(a.link.value+" "+b.link.value);    // a.link.value will be null
+    }
+
+    public static Q15 setIt(final Q15 x, final Q15 y){
+        x.link = y.link;
+        return x;
+    }
+
+}
---
+++ src/main/java/com/ocp/java/mock/test1/Q16.java
@@ -0,0 +1,30 @@
+package com.ocp.java.mock.test1;
+
+interface I{
+}
+
+class A implements I{
+}
+
+class B extends A {
+}
+
+class C extends B{
+}
+
+public class Q16 {
+
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+
+
+        a = (B)(I)b;        // okay
+
+        b = (B)(I) a;       // runtime failure A is not B
+
+        // a = (I) b;       // compilation failure - assigning super type (I) to subtype (A)
+
+        I i = (C) a;        // Run time failure - A is not C
+    }
+}
