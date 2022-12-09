fa13a8fdf8a091d8e30aafa8e251e875e25c7a8a
sanjiv <sanjiv.katiyar@gmail.com>
2022-12-09 05:04:01 -0700

    ok

--- src/main/java/com/ocp/_19/AccessModifiers/Test.java
+++ src/main/java/com/ocp/_19/AccessModifiers/Test.java
@@ -21,4 +21,15 @@ There are four types of access modifiers in Java
 
 - public
 
+
+
+
+Below is the table with details on access modifier.
+
+Keyword	Applicable To	Who can Access
+private	Data members, methods, and inner class	All members from the same class only
+(No keyword, usually we call it default)	Data members, methods, classes, and interfaces	All classes from within the same package
+protected	Data members, methods, and inner class	All classes from within the same package as well as all subclasses i.e. even subclasses residing in a different package
+public	Data members, methods, classes, and interfaces	Any class
+
  */
